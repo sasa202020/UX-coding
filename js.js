12832e302d4bc85d050342109fcff4f12a43e14d
sasa202020 <sosiso4ki64@gmail.com>
2022-03-14 19:12:14 +0100

    first steps

---
+++ js.js
@@ -0,0 +1,19 @@
+function initMap() {
+    const myLatlng = { lat: -25.363, lng: 131.044 };
+    
+    
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: myLatlng,
+    });
+    
+    
+    const marker = new google.maps.Marker({
+      position: myLatlng,
+      map,
+      title: "Click to zoom",
+    });
+  
+
+}
+
