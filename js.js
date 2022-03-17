64fee81f5ac23a17feccc9f0895982f168667e89
sasa202020 <sosiso4ki64@gmail.com>
2022-03-17 16:41:21 +0100

    version before step implementing markers

--- js.js
+++ js.js
@@ -1,8 +1,8 @@
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    mapId: '86ad4126f5ea124b',
     center: {lat: 55.45802, lng: 12.18214},
     zoom: 12,
+    mapId: '86ad4126f5ea124b'
     });
 }
 
