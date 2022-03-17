819b5dcc119b3846b16781c050067f2d1a1352e9
sasa202020 <sosiso4ki64@gmail.com>
2022-03-17 18:43:52 +0100

    added all 3 working markers

--- js.js
+++ js.js
@@ -5,35 +5,57 @@ function initMap() {
     mapId: '86ad4126f5ea124b'
     });
     const markers = [                                    //adding array of markers
-      "Test title0",
+      [
+        "Test title0",
       55.45802,
       12.18214,
       "/pics/bluepinpoint.png",
       28,
       43
-    ];
+      ],
 
+      [
+        "Køge Svømmeland",
+        55.48472451959858,
+        12.17600994876877,
+        "/pics/bluepinpoint.png",
+        28,
+        43
+      ],
+
+      [
+        "Køge Golf Club",
+        55.42787041249714,
+        12.181131837151261,
+        "/pics/bluepinpoint.png",
+        28,
+        43
+      ]
+    ];
+//55.42787041249714, 12.181131837151261
       for(let i = 0; i<markers.length; i++){
         const currMarker = markers[i];
 
+        const marker = new google.maps.Marker({              // adding marker
+          position: { lat: currMarker[1], lng: currMarker[2] },
+          map,
+          title: currMarker[0],
+          icon: {
+            url: currMarker[3],
+            scaledSize: new google.maps.Size(currMarker[4], currMarker[5])
+          },
+          animation: google.maps.Animation.DROP   //just for test better for delete i think
+        });
+        const infowindow = new google.maps.InfoWindow({      //adding info window
+          content: "will insert info after here will insert info after here will insert info after here will insert info after here",
+        });
+        marker.addListener("click", () =>{          // call for opening info window
+          infowindow.open(map,marker);
+        });
+
       }
 
-    const marker = new google.maps.Marker({              // adding marker
-      position: { lat: 55.45802, lng: 12.18214 },
-      map,
-      title: "Test title0",
-      icon: {
-        url: "/pics/bluepinpoint.png",
-        scaledSize: new google.maps.Size(28, 43)
-      },
-      animation: google.maps.Animation.DROP   //just for test better for delete i think
-    });
-    const infowindow = new google.maps.InfoWindow({      //adding info window
-      content: "insert content for location insert content for location insert content for location ",
-    });
-    marker.addListener("click", () =>{          // call for opening info window
-      infowindow.open(map,marker);
-    });
+   
 }
 
 
