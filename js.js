60e440557b4bf5de1ae37f75a6f99ad5329797bb
sasa202020 <sosiso4ki64@gmail.com>
2022-03-17 12:48:33 +0100

    trying to add my own map

--- js.js
+++ js.js
@@ -1,19 +1,12 @@
 function initMap() {
-    const myLatlng = { lat: -25.363, lng: 131.044 };
-    
-    
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: myLatlng,
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8,
+    mapId: '86ad4126f5ea124b'
     });
-    
-    
-    const marker = new google.maps.Marker({
-      position: myLatlng,
-      map,
-      title: "Click to zoom",
     });
   
 
 }
+
 
