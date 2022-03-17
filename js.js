e69a29269e1ca099354588de9cc6dbecdb045182
sasa202020 <sosiso4ki64@gmail.com>
2022-03-17 17:10:03 +0100

    added 1 working pinpoint on location

--- js.js
+++ js.js
@@ -4,6 +4,15 @@ function initMap() {
     zoom: 12,
     mapId: '86ad4126f5ea124b'
     });
+    const marker = new google.maps.Marker({
+      position: { lat: 55.45802, lng: 12.18214 },
+      map,
+      title: "Test title",
+      icon: {
+        url: "/pics/bluepinpoint.png",
+        scaledSize: new google.maps.Size(28, 43)
+      }
+    });
 }
 
 
