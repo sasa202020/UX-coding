4426f27beae81102e142effc5275b91b2acc4df3
sasa202020 <sosiso4ki64@gmail.com>
2022-03-17 18:13:19 +0100

    started 4 loop

--- js.js
+++ js.js
@@ -4,10 +4,24 @@ function initMap() {
     zoom: 12,
     mapId: '86ad4126f5ea124b'
     });
+    const markers = [                                    //adding array of markers
+      "Test title0",
+      55.45802,
+      12.18214,
+      "/pics/bluepinpoint.png",
+      28,
+      43
+    ];
+
+      for(let i = 0; i<markers.length; i++){
+        const currMarker = markers[i];
+
+      }
+
     const marker = new google.maps.Marker({              // adding marker
       position: { lat: 55.45802, lng: 12.18214 },
       map,
-      title: "Test title",
+      title: "Test title0",
       icon: {
         url: "/pics/bluepinpoint.png",
         scaledSize: new google.maps.Size(28, 43)
@@ -15,7 +29,7 @@ function initMap() {
       animation: google.maps.Animation.DROP   //just for test better for delete i think
     });
     const infowindow = new google.maps.InfoWindow({      //adding info window
-      content: "insert content for location insert content for location insert content for location insert content for location insert content for location insert content for location insert content for location insert content for location insert content for location insert content for location insert content for location insert content for location insert content for location insert content for location",
+      content: "insert content for location insert content for location insert content for location ",
     });
     marker.addListener("click", () =>{          // call for opening info window
       infowindow.open(map,marker);
