6afa53ea76464ec608a9d275993abeaf867772d2
sasa202020 <sosiso4ki64@gmail.com>
2022-03-20 18:43:26 +0100

    finished style for mobile

--- js.js
+++ js.js
@@ -67,7 +67,7 @@ let marker1 = {
           animation: google.maps.Animation.DROP   //just for test better for delete i think
         });
         const infowindow = new google.maps.InfoWindow({      //adding info window
-          content: "will insert info after here will insert info after here will insert info after here will insert info after here",
+          content: "<img src='./pics/restaurant.jpg' width='200px';><h6>Restaurant Arken</h6><img class='icon1' src='./pics/find.png' width='40px'><p class='text1'>Bådehavnen 21, 4600 Køge</p><img class='icon2' src='./pics/phone.png' width='40px'><p class='text2'>56 66 05 05</p><img class='icon3' src='./pics/mail.png' width='40px'><p class='text3'>kontakt@restaurant-arken.dk</p>"
         });
         marker.addListener("click", () =>{          // call for opening info window
           infowindow.open(map,marker);
