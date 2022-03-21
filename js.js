f16cea30d3c1c3dedde112c692a508afec099da5
Angelov-digital <96943583+Angelov-digital@users.noreply.github.com>
2022-03-21 20:36:42 +0100

    update

--- js.js
+++ js.js
@@ -1,3 +1,9 @@
+
+
+
+
+
+
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {   // call for map
     center: {lat: 55.45802, lng: 12.18214},
@@ -78,4 +84,29 @@ let marker1 = {
    
 }
 
+const open_btn = document.querySelector('.open-btn')
+const close_btn = document.querySelector('.close-btn')
+const popup = document.querySelector('.popup')
+const main_popup = document.querySelector('.main-popup')
 
+open_btn.addEventListener('click', () => {
+  console.log('tets')
+	popup.style.display = 'flex';
+	main_popup.style.cssText = 'animation:slide-in .5s ease; animation-fill-mode: forwards;';
+});
+
+close_btn.addEventListener('click', () => {
+	main_popup.style.cssText = 'animation:slide-out .5s ease; animation-fill-mode: forwards;';
+	setTimeout(() => {
+		popup.style.display = 'none';
+	}, 500);
+});
+
+window.addEventListener('click', (e) => {
+	if (e.target == document.querySelector('.popup-overlay')) {
+		main_popup.style.cssText = 'animation:slide-out .5s ease; animation-fill-mode: forwards;';
+		setTimeout(() => {
+			popup.style.display = 'none';
+		}, 500);
+	}
+});
